af87f335bcd69564891a39bda0ddeee10ced3d5a
MaximePortable <maxime.sage97@gmail.com>
2022-04-11 10:51:29 +0200

    Test commit

--- SemaineProg/src/projetLejos/SayHi.java
+++ SemaineProg/src/projetLejos/SayHi.java
@@ -4,8 +4,8 @@ public class SayHi {
 
 	public static void main(String[] args) {
 		System.out.println("Coucou");
-		for (int i=0; i<1000000000; i++) {
-			
+		for (int i = 0; i < 1000000000; i++) {
+
 		}
 	}
 }
