dd998d2232002fb32320593d5ecffd5b57ee8507
MaximePortable <maxime.sage97@gmail.com>
2022-04-11 11:10:52 +0200

    Message pour Maxiiiiime

--- SemaineProg/src/projetLejos/SayHi.java
+++ SemaineProg/src/projetLejos/SayHi.java
@@ -3,7 +3,7 @@ package projetLejos;
 public class SayHi {
 
 	public static void main(String[] args) {
-		System.out.println("Coucou");
+		System.out.println("Maxime est un homme viril, qui vote à droite");
 		for (int i = 0; i < 1000000000; i++) {
 
 		}
